5a3d74713ecef2ccfd1b3378337c38eb72731bf1
Alice Feng <feng.a.g@gmail.com>
2018-06-14 22:32:38 -0400

    Add location properties to Player class and render Player on screen

--- js/app.js
+++ js/app.js
@@ -36,13 +36,22 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    this.sprite = 'images/char-princess-girl.png';
+    this.x = x;
+    this.y = y;
+}
 
+// Draw player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(60,60,40), new Enemy(0, 140, 100), new Enemy(-60, 220, 80)];
-var player;
+var allEnemies = [new Enemy(60, 60, 40), new Enemy(0, 140, 100), new Enemy(-60, 220, 80)];
+var player = new Player(200, 404);
 
 
 // This listens for key presses and sends the keys to your
