5d662918f82a3b7e637e61ff8daeb367d6942cbb
Alice Feng <feng.a.g@gmail.com>
2018-06-14 22:04:29 -0400

    Add location and speed properties to Enemy class and implement update method so enemy moves across the screen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // set initial location and speed of the enemy
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x > 505) {
+        // if enemy has reached the end of the board, it should loop back around
+        this.x = -101;  // set x to -101 so bug is off-screen
+    }
+    else {
+        this.x = this.x + (this.speed * dt);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -29,7 +41,8 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(60,60,40), new Enemy(0, 140, 100), new Enemy(-60, 220, 80)];
+var player;
 
 
 // This listens for key presses and sends the keys to your
